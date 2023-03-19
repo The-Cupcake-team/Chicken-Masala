3022eb410f4f5f1cc35bf7eaea0533ba955c2ba4
Ethaar hussien <ethaar7735@gmail.com>
2023-03-19 21:44:29 +0300

    create base fragment

--- app/src/main/java/com/cupcake/chickenmasala/BaseFragment.kt
+++ app/src/main/java/com/cupcake/chickenmasala/BaseFragment.kt
@@ -8,9 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragment<VB : ViewBinding > : Fragment() {
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
     abstract val LOG_TAG: String
-    abstract val bindingInflater:(LayoutInflater,ViewGroup , Boolean )-> VB
+    abstract val bindingInflater: (LayoutInflater, ViewGroup, Boolean) -> VB
     private var _binding: ViewBinding? = null
     protected val binding get() = _binding!!
     override fun onCreateView(
@@ -18,16 +18,12 @@ abstract class BaseFragment<VB : ViewBinding > : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = bindingInflater(inflater, requireNotNull(container),false)
-        setup()
-        addCallbacks()
+        _binding = bindingInflater(inflater, requireNotNull(container), false)
         return binding.root
     }
 
-    abstract fun setup()
-    abstract fun addCallbacks()
-    protected fun log(value: String ){
-        Log.v(LOG_TAG,value)
+    protected fun log(value: String) {
+        Log.v(LOG_TAG, value)
     }
 
 }
