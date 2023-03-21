654f1b274650744bbf713337ed4bd80bc15ebe89
TarekIdrees <tarekdrias321@gmail.com>
2023-03-21 11:16:18 +0200

    cast view binding

--- app/src/main/java/com/cupcake/chickenmasala/BaseFragment.kt
+++ app/src/main/java/com/cupcake/chickenmasala/BaseFragment.kt
@@ -12,7 +12,7 @@ abstract class BaseFragment<VB : ViewBinding> : Fragment() {
     abstract val LOG_TAG: String
     abstract val bindingInflater: (LayoutInflater, ViewGroup, Boolean) -> VB
     private var _binding: ViewBinding? = null
-    protected val binding get() = _binding!!
+    protected val binding get() = _binding!! as VB
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
