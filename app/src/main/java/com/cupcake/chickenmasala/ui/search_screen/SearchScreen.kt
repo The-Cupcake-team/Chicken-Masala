e68c6fea4551962530dc9ce021fff5ba0067e738
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-03-21 19:44:35 +0200

    ui: add bottom sheet

--- app/src/main/java/com/cupcake/chickenmasala/ui/search_screen/SearchScreen.kt
+++ app/src/main/java/com/cupcake/chickenmasala/ui/search_screen/SearchScreen.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.cupcake.chickenmasala.BaseFragment
+import com.cupcake.chickenmasala.R
 import com.cupcake.chickenmasala.databinding.FragmentSearchScreenBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.chip.ChipGroup
 
 
 class SearchScreen : BaseFragment<FragmentSearchScreenBinding>() {
@@ -16,12 +19,33 @@ class SearchScreen : BaseFragment<FragmentSearchScreenBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupBottomSheet()
+    }
+
+    private fun setupBottomSheet() {
         binding.searchBox.setEndIconOnClickListener {
-           showFilterSheet()
+            showFilterSheet()
         }
     }
 
     private fun showFilterSheet() {
+        val dialog = BottomSheetDialog(requireContext())
+        val view = layoutInflater.inflate(R.layout.filter_sheet_card, null)
+        with(view) {
+            findViewById<MaterialButton>(R.id.btn_apply).setOnClickListener {
+                filterList()
+                dialog.dismiss()
+            }
+            findViewById<MaterialButton>(R.id.btn_clear).setOnClickListener {
+                findViewById<ChipGroup>(R.id.time_group).clearCheck()
+                findViewById<ChipGroup>(R.id.ingredients_group).clearCheck()
+            }
+        }
+        dialog.setContentView(view)
+        dialog.show()
+    }
+
+    private fun filterList() {
 
     }
 
