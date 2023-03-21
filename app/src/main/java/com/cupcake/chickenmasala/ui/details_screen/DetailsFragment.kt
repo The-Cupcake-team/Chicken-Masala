e8298eff4801dbbac5b8bdd081f3b0128a953b59
AndrewAboalhana <hana_hany6@yahoo.com>
2023-03-21 23:30:57 +0200

    ui: update spaces in xml and put details frgament in ui package

--- app/src/main/java/com/cupcake/chickenmasala/ui/details_screen/DetailsFragment.kt
+++ app/src/main/java/com/cupcake/chickenmasala/ui/details_screen/DetailsFragment.kt
@@ -1,10 +1,11 @@
-package com.cupcake.chickenmasala
+package com.cupcake.chickenmasala.ui.details_screen
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.cupcake.chickenmasala.R
 
 class DetailsFragment : Fragment() {
 
