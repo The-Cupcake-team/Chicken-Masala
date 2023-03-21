9b8e417cb7bde77ac64938ad0bd002d8ac9277d0
Zainab Mohammed <st2020021518@student.alkafeel.edu.iq>
2023-03-22 02:32:00 +0300

    create splash screen

--- app/src/main/java/com/cupcake/chickenmasala/ui/MainActivity.kt
+++ app/src/main/java/com/cupcake/chickenmasala/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import com.cupcake.chickenmasala.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_ChickenMasala)
         setContentView(R.layout.activity_main)
     }
 }
