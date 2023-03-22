7feec31d857a554b3cee89bf7924c777e870ca80
ameeramjed <pr.ameer98@gmail.com>
2023-03-22 06:40:53 +0300

    fix conflict issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,11 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    packagingOptions {
+        exclude("META-INF/LICENSE.md")
+        exclude("META-INF/NOTICE.md")
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -45,8 +50,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.core:core-ktx:+")
-    implementation("androidx.databinding:databinding-compiler-common:7.1.2")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.databinding:databinding-compiler-common:7.4.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
