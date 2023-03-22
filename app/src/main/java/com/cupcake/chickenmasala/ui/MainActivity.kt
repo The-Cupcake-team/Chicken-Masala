a529279decf940d34b0a66c199a298e4ad082544
Mustafa Sabe Alarab <mustafa777alarab@gmail.com>
2023-03-22 11:45:11 +0100

    add data binding implement fot main activity

--- app/src/main/java/com/cupcake/chickenmasala/ui/MainActivity.kt
+++ app/src/main/java/com/cupcake/chickenmasala/ui/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.cupcake.chickenmasala.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.cupcake.chickenmasala.R
+import androidx.appcompat.app.AppCompatActivity
+import com.cupcake.chickenmasala.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
