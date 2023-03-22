c3c5fcb327a6148dc79d7d483898c8bd21dcba93
AndrewAboalhana <hana_hany6@yahoo.com>
2023-03-22 18:39:32 +0200

    fix error in open website function

--- app/src/main/java/com/cupcake/chickenmasala/ui/fragment/DetailsFragment.kt
+++ app/src/main/java/com/cupcake/chickenmasala/ui/fragment/DetailsFragment.kt
@@ -3,15 +3,13 @@ package com.cupcake.chickenmasala.ui.fragment
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.cupcake.chickenmasala.BaseFragment
-import com.cupcake.chickenmasala.R
 import com.cupcake.chickenmasala.databinding.FragmentDetailsBinding
+import com.cupcake.chickenmasala.ui.base.BaseFragment
 
-class DetailsFragment :BaseFragment<FragmentDetailsBinding>(){
+class DetailsFragment : BaseFragment<FragmentDetailsBinding>(){
 
     override val LOG_TAG: String = "Details_Fragment"
 
@@ -21,14 +19,13 @@ class DetailsFragment :BaseFragment<FragmentDetailsBinding>(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        openWebsite("www.google.com")
+        openWebsite("https://www.google.com")
     }
 
     private fun openWebsite(url:String){
         binding.moreDetailsButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse(url)
-            startActivity(intent)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            activity?.startActivity(intent)
         }
     }
 }
