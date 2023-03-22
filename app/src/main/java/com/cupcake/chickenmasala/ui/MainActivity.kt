a6e19784b07ada0a3d0640ea577111075519186e
Mustafa Sabe Alarab <mustafa777alarab@gmail.com>
2023-03-22 13:58:02 +0100

    initialize objects and make them private

--- app/src/main/java/com/cupcake/chickenmasala/ui/MainActivity.kt
+++ app/src/main/java/com/cupcake/chickenmasala/ui/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.cupcake.chickenmasala.ui
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.cupcake.chickenmasala.R
@@ -11,6 +10,10 @@ import com.cupcake.chickenmasala.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private val homeFragment = HomeFragment()
+    private val searchFragment = SearchFragment()
+    private val cuisineFragment = CuisineFragment()
+    private val historyFragment = HistoryFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,19 +25,19 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigationBar.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.home_fragment -> {
-                    replaceFragment(HomeFragment())
+                    replaceFragment(homeFragment)
                     true
                 }
                 R.id.search_fragment -> {
-                    replaceFragment(SearchFragment())
+                    replaceFragment(searchFragment)
                     true
                 }
                 R.id.cuisine_fragment -> {
-                    replaceFragment(CuisineFragment())
+                    replaceFragment(cuisineFragment)
                     true
                 }
                 R.id.history_fragment -> {
-                    replaceFragment(HistoryFragment())
+                    replaceFragment(historyFragment)
                     true
                 }
                 else -> false
