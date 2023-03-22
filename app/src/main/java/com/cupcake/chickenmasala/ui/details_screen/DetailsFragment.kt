a579e2b368668acbdec81b5f67418d75763bc715
AndrewAboalhana <hana_hany6@yahoo.com>
2023-03-22 18:08:14 +0200

    add function open website

--- app/src/main/java/com/cupcake/chickenmasala/ui/details_screen/DetailsFragment.kt
+++ app/src/main/java/com/cupcake/chickenmasala/ui/details_screen/DetailsFragment.kt
@@ -1,25 +1,34 @@
 package com.cupcake.chickenmasala.ui.details_screen
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.cupcake.chickenmasala.BaseFragment
 import com.cupcake.chickenmasala.R
+import com.cupcake.chickenmasala.databinding.FragmentDetailsBinding
 
-class DetailsFragment : Fragment() {
+class DetailsFragment :BaseFragment<FragmentDetailsBinding>(){
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override val LOG_TAG: String = "Details_Fragment"
+
+    override val bindingInflater: (LayoutInflater, ViewGroup, Boolean) ->
+    FragmentDetailsBinding = FragmentDetailsBinding::inflate
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        openWebsite("www.google.com")
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    private fun openWebsite(url:String){
+        binding.moreDetailsButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
     }
-
-
 }
