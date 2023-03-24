284b34bcb37077c0c898cbf4c814de79f64a9f58
David samuel <davidsamuelramsis@gmail.com>
2023-03-24 16:38:59 +0200

    ui: convert to sheet binding

--- app/src/main/java/com/cupcake/chickenmasala/ui/fragment/SearchFragment.kt
+++ app/src/main/java/com/cupcake/chickenmasala/ui/fragment/SearchFragment.kt
@@ -6,8 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.cupcake.chickenmasala.ui.base.BaseFragment
 import com.cupcake.chickenmasala.R
+import com.cupcake.chickenmasala.databinding.FilterSheetCardBinding
 import com.cupcake.chickenmasala.databinding.FragmentSearchBinding
 import com.cupcake.chickenmasala.ui.activity.HomeActivity
+import com.cupcake.chickenmasala.usecase.search.FilterByIngredientsUseCase
+import com.cupcake.chickenmasala.usecase.search.FilterByTimeRangeUseCase
+import com.cupcake.chickenmasala.usecase.search.SearchByRecipeNameUseCase
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.chip.ChipGroup
@@ -18,14 +22,23 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>() {
     override val bindingInflater: (LayoutInflater, ViewGroup, Boolean) -> FragmentSearchBinding =
         FragmentSearchBinding::inflate
 
+    val dataManager by lazy { (activity as HomeActivity).dataManager }
+    private val filterByIngredientsUseCase by lazy { FilterByIngredientsUseCase(dataManager) }
+    private val filterByTimeRangeUseCase by lazy { FilterByTimeRangeUseCase(dataManager) }
+    private val searchByRecipeNameUseCase by lazy { SearchByRecipeNameUseCase(dataManager) }
+
+    private val sheetBinding by lazy { FilterSheetCardBinding.inflate(layoutInflater) }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupBottomSheet()
 
-        (activity as HomeActivity).dataManager.getRecipes().forEach {
-            log(it.id.toString())
-        }
+        //log(filterByIngredientsUseCase(listOf(getString(R.string.onion), getString(R.string.eggs), getString(R.string.green_chillies), getString(R.string.salt))).toString())
 
+        //log(filterByTimeRangeUseCase(listOf(0..30, 30..60))?.map { it.totalTimeInMin }.toString ())
+
+        // log(searchByRecipeNameUseCase(recipeName = "tomato")?.map { it.translatedRecipeName }.toString())
     }
 
     private fun setupBottomSheet() {
@@ -36,18 +49,17 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>() {
 
     private fun showFilterSheet() {
         val dialog = BottomSheetDialog(requireContext())
-        val view = layoutInflater.inflate(R.layout.filter_sheet_card, null)
-        with(view) {
-            findViewById<MaterialButton>(R.id.btn_apply).setOnClickListener {
+        with(sheetBinding) {
+            btnApply.setOnClickListener {
                 filterList()
                 dialog.dismiss()
             }
-            findViewById<MaterialButton>(R.id.btn_clear).setOnClickListener {
-                findViewById<ChipGroup>(R.id.time_group).clearCheck()
-                findViewById<ChipGroup>(R.id.ingredients_group).clearCheck()
+            btnClear.setOnClickListener {
+                timeGroup.clearCheck()
+                ingredientsGroup.clearCheck()
             }
         }
-        dialog.setContentView(view)
+        dialog.setContentView(sheetBinding.root)
         dialog.show()
     }
 
