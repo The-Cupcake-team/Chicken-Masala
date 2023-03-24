d558ca8fcccc71f67b6c542a17cf2ac9a27d4376
Ethaar hussien <ethaar7735@gmail.com>
2023-03-25 00:52:37 +0300

    add dependency for glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.fragment:fragment-ktx:1.5.5")
 
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+
 }
