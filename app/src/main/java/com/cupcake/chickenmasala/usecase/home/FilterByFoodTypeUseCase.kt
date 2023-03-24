5cda2b030431ed329b2251c79847c2198b354c12
Ethaar hussien <ethaar7735@gmail.com>
2023-03-24 17:00:42 +0300

    create home use cases

--- app/src/main/java/com/cupcake/chickenmasala/usecase/home/FilterByFoodTypeUseCase.kt
+++ app/src/main/java/com/cupcake/chickenmasala/usecase/home/FilterByFoodTypeUseCase.kt
@@ -3,9 +3,9 @@ package com.cupcake.chickenmasala.usecase.home
 import com.cupcake.chickenmasala.data.model.Recipe
 import com.cupcake.chickenmasala.usecase.DataManager
 
-class GetRecentFoodUseCase(private val dataManager: DataManager) {
+class FilterByFoodTypeUseCase(private val dataManager: DataManager){
 
-    operator fun invoke(): List<Recipe> {
+    operator fun invoke():List<Recipe> {
         return dataManager.getRecipes()
     }
 
