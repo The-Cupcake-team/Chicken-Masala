123826591aaeec7b616dd15d0d9de260f7a9cfc9
Ethaar hussien <ethaar7735@gmail.com>
2023-03-24 15:33:39 +0300

    add class use cases

---
+++ app/src/main/java/com/cupcake/chickenmasala/usecase/home/GetRecentFoodUseCase.kt
@@ -0,0 +1,12 @@
+package com.cupcake.chickenmasala.usecase.home
+
+import com.cupcake.chickenmasala.usecase.DataManager
+
+class GetRecentFoodUseCase(private val dataManager: DataManager) {
+
+    operator fun invoke(){
+
+    }
+
+
+}
