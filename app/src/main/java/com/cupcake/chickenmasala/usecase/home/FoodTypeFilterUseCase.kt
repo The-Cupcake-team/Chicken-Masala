cbd3762b6cc80a9979b12f63bb2b704cd6dec5cf
أســـــاور <xyzas.7510@gmail.com>
2023-03-25 05:45:05 +0300

    food type filter use case done

--- app/src/main/java/com/cupcake/chickenmasala/usecase/home/FoodTypeFilterUseCase.kt
+++ app/src/main/java/com/cupcake/chickenmasala/usecase/home/FoodTypeFilterUseCase.kt
@@ -2,12 +2,8 @@ package com.cupcake.chickenmasala.usecase.home
 
 import com.cupcake.chickenmasala.data.model.Recipe
 import com.cupcake.chickenmasala.usecase.DataManager
-
-class FilterByFoodTypeUseCase(private val dataManager: DataManager){
-
-    operator fun invoke():List<Recipe> {
-        return dataManager.getRecipes()
-    }
-
-
+class FoodTypeFilterUseCase(
+    private val dataManager: DataManager
+    ){
+    operator fun invoke():List<Recipe> = dataManager.getRecipes()
 }
