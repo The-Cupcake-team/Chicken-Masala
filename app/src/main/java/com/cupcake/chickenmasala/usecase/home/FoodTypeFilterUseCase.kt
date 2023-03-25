0057fa2e0f71b6090106aeeef943ca14a8146e49
أســـــاور <xyzas.7510@gmail.com>
2023-03-25 17:11:17 +0300

    change to repository

--- app/src/main/java/com/cupcake/chickenmasala/usecase/home/FoodTypeFilterUseCase.kt
+++ app/src/main/java/com/cupcake/chickenmasala/usecase/home/FoodTypeFilterUseCase.kt
@@ -1,9 +1,13 @@
 package com.cupcake.chickenmasala.usecase.home
 
 import com.cupcake.chickenmasala.data.model.Recipe
-import com.cupcake.chickenmasala.usecase.DataManager
+import com.cupcake.chickenmasala.usecase.Repository
+
 class FoodTypeFilterUseCase(
-    private val dataManager: DataManager
-    ){
-    operator fun invoke():List<Recipe> = dataManager.getRecipes()
+    private val repository: Repository
+) {
+    operator fun invoke(ingredient: String): List<Recipe> {
+        return repository.getRecipes()
+            .filter { ingredient in it.cleanedIngredients }
+    }
 }
