25a0d4df716f7aba930a8bdf726b96ee0d4e0f21
Ethaar hussien <ethaar7735@gmail.com>
2023-03-25 18:13:39 +0300

    refactor horizontal recipe adapter

--- app/src/main/java/com/cupcake/chickenmasala/ui/adapter/home/HorizontalRecipeAdapter.kt
+++ app/src/main/java/com/cupcake/chickenmasala/ui/adapter/home/HorizontalRecipeAdapter.kt
@@ -9,16 +9,19 @@ import com.bumptech.glide.Glide
 import com.cupcake.chickenmasala.R
 import com.cupcake.chickenmasala.data.model.Recipe
 import com.cupcake.chickenmasala.databinding.SearchCardViewBinding
-import com.cupcake.chickenmasala.utill.setImage
 
 
-class HorizontalRecipeAdapter(private var recipeList: List<Recipe>, private val listener : RecentFoodInteractionClickListener)
-    : RecyclerView.Adapter<HorizontalRecipeAdapter.HorizontalRecipeViewHolder>() {
+class HorizontalRecipeAdapter(
+    private var recipeList: List<Recipe>
+    ) : RecyclerView.Adapter<HorizontalRecipeAdapter.HorizontalRecipeViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HorizontalRecipeViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.search_card_view, parent, false)
         return HorizontalRecipeViewHolder(view)
     }
+
+
     override fun onBindViewHolder(holder: HorizontalRecipeViewHolder, position: Int) {
         val currentHome = recipeList[position]
 
@@ -27,17 +30,25 @@ class HorizontalRecipeAdapter(private var recipeList: List<Recipe>, private val 
             dishName.text = currentHome.translatedRecipeName
             cuisineName.text = currentHome.cuisine
             prepareTime.text = currentHome.totalTimeInMin.toString()
-            root.setOnClickListener { listener.onClickRecentFoodItem(currentHome.id) }
+
         }
     }
 
+
     override fun getItemCount() = recipeList.size
+
+    private fun ImageView.setImage(url: String?) {
+        Glide.with(context).load(url)
+            .into(this)
+    }
+
 
     class HorizontalRecipeViewHolder(viewItem: View) : RecyclerView.ViewHolder(viewItem) {
         val binding = SearchCardViewBinding.bind(viewItem)
+
     }
 
-    fun setData(newRecipeList: List<Recipe>){
+    fun setData(newRecipeList: List<Recipe>) {
         recipeList = newRecipeList
         notifyDataSetChanged()
     }
