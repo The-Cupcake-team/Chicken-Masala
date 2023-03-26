932c4f373e2ca9e5bdae1a273a229470c8ee0ba3
ameeramjed <pr.ameer98@gmail.com>
2023-03-26 17:07:15 +0300

    improve code with replace lotti to gif

--- app/src/main/java/com/cupcake/chickenmasala/ui/adapter/home/HorizontalRecipeAdapter.kt
+++ app/src/main/java/com/cupcake/chickenmasala/ui/adapter/home/HorizontalRecipeAdapter.kt
@@ -3,12 +3,11 @@ package com.cupcake.chickenmasala.ui.adapter.home
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.cupcake.chickenmasala.R
 import com.cupcake.chickenmasala.data.model.Recipe
 import com.cupcake.chickenmasala.databinding.SearchCardViewBinding
+import com.cupcake.chickenmasala.utill.setImage
 
 
 class HorizontalRecipeAdapter(private var recipeList: List<Recipe>) : RecyclerView.Adapter<HorizontalRecipeAdapter.HorizontalRecipeViewHolder>() {
@@ -30,10 +29,7 @@ class HorizontalRecipeAdapter(private var recipeList: List<Recipe>) : RecyclerVi
     }
 
     override fun getItemCount() = recipeList.size
-    private fun ImageView.setImage(url: String?) {
-        Glide.with(context).load(url)
-            .into(this)
-    }
+
 
     class HorizontalRecipeViewHolder(viewItem: View) : RecyclerView.ViewHolder(viewItem) {
         val binding = SearchCardViewBinding.bind(viewItem)
--- app/src/main/java/com/cupcake/chickenmasala/utill/Extention.kt
+++ app/src/main/java/com/cupcake/chickenmasala/utill/Extention.kt
@@ -1,46 +1,15 @@
 package com.cupcake.chickenmasala.utill
 
-import android.graphics.drawable.Drawable
-import android.view.View
 import android.widget.ImageView
-import com.airbnb.lottie.LottieAnimationView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
+import com.cupcake.chickenmasala.R
 
-fun ImageView.loadImage(url: String?, lottieView: LottieAnimationView?) {
+fun ImageView.setImage(url: String) {
 
-    lottieView?.visibility = View.VISIBLE
+    Glide.with(context).load(url)
+        .thumbnail(Glide.with(context).load(R.raw.loading))
+        .fitCenter()
+        .centerCrop()
+        .into(this);
 
-    val glide = Glide
-        .with(context)
-        .load(url)
-
-    val listener = object : RequestListener<Drawable> {
-
-        override fun onLoadFailed(
-            e: GlideException?,
-            model: Any?,
-            target: Target<Drawable>?,
-            isFirstResource: Boolean
-        ): Boolean {
-            return false
-        }
-
-        override fun onResourceReady(
-            resource: Drawable?,
-            model: Any?,
-            target: Target<Drawable>?,
-            dataSource: DataSource?,
-            isFirstResource: Boolean
-        ): Boolean {
-            lottieView?.visibility = View.GONE
-            lottieView?.cancelAnimation()
-            return true
-        }
-    }
-
-    glide.listener(listener).into(this)
 }
