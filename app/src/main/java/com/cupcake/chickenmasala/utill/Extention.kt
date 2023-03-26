cac6a461dc49cc1b93b868009b375e9409bc2d5c
Mustafa Sabe Alarab <mustafa777alarab@gmail.com>
2023-03-26 13:11:46 +0200

    refactor function that loading image and visible animation with lottie library

--- app/src/main/java/com/cupcake/chickenmasala/utill/Extention.kt
+++ app/src/main/java/com/cupcake/chickenmasala/utill/Extention.kt
@@ -1,14 +1,46 @@
 package com.cupcake.chickenmasala.utill
 
+import android.graphics.drawable.Drawable
+import android.view.View
 import android.widget.ImageView
+import com.airbnb.lottie.LottieAnimationView
 import com.bumptech.glide.Glide
-import com.cupcake.chickenmasala.R
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
+fun ImageView.loadImage(url: String?, lottieView: LottieAnimationView?) {
 
-fun ImageView.setImage(url: String?) {
-    Glide.with(context)
+    lottieView?.visibility = View.VISIBLE
+
+    val glide = Glide
+        .with(context)
         .load(url)
-        .placeholder(R.drawable.ic_launcher_foreground)
-        .error(R.drawable.ic_launcher_background)
-        .into(this)
+
+    val listener = object : RequestListener<Drawable> {
+
+        override fun onLoadFailed(
+            e: GlideException?,
+            model: Any?,
+            target: Target<Drawable>?,
+            isFirstResource: Boolean
+        ): Boolean {
+            return false
+        }
+
+        override fun onResourceReady(
+            resource: Drawable?,
+            model: Any?,
+            target: Target<Drawable>?,
+            dataSource: DataSource?,
+            isFirstResource: Boolean
+        ): Boolean {
+            lottieView?.visibility = View.GONE
+            lottieView?.cancelAnimation()
+            return true
+        }
+    }
+
+    glide.listener(listener).into(this)
 }
