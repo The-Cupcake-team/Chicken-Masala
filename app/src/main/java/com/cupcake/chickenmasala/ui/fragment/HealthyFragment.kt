e2d0ce7264ea2a08440656e7466c6eaa6d017b73
Abanoub-Nagy <abanoubnagy24@gmail.com>
2023-03-26 06:17:07 +0200

    implement code for healthy fragment

--- app/src/main/java/com/cupcake/chickenmasala/ui/fragment/HealthyFragment.kt
+++ app/src/main/java/com/cupcake/chickenmasala/ui/fragment/HealthyFragment.kt
@@ -5,20 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.cupcake.chickenmasala.data.RepositoryImpl
+import com.cupcake.chickenmasala.data.model.HealthAdvice
 import com.cupcake.chickenmasala.databinding.FragmentHealthyBinding
 import com.cupcake.chickenmasala.ui.base.BaseFragment
 import com.cupcake.chickenmasala.usecase.Repository
+import com.cupcake.chickenmasala.usecase.healthy.GetHealthyAdviceByIDUseCase
 import com.cupcake.chickenmasala.utill.DataSourceProvider
+import com.cupcake.chickenmasala.utill.setImage
 
 class HealthyFragment : BaseFragment<FragmentHealthyBinding>() {
     override val LOG_TAG = "HealthFragment"
 
     override val bindingInflater: (LayoutInflater, ViewGroup, Boolean) -> FragmentHealthyBinding =
         FragmentHealthyBinding::inflate
-    private lateinit var repository: Repository
+
+    private val repository: Repository by lazy {
+        RepositoryImpl(DataSourceProvider.getDataSource(requireActivity().application))
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        repository = RepositoryImpl(DataSourceProvider.getDataSource(requireActivity().application))
+
+        val adviceID = arguments?.getInt("adviceID")
+        adviceID?.let {
+
+        }
+        initialData(1)
+
+    }
+
+    private fun initialData(adviceID: Int) {
+        val advice = GetHealthyAdviceByIDUseCase(repository).invoke(adviceID)
+        bindViews(advice)
+    }
+
+    private fun bindViews(healthyAdvice: HealthAdvice) {
+        binding.toolbar.title = healthyAdvice.title
+        binding.healthyImage.setImage(healthyAdvice.imageUrl)
+        binding.healthyDescription.text = healthyAdvice.description
     }
 }
