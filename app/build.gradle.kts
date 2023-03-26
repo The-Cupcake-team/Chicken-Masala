442611ade87301735929b152e959e172990a8264
Abanoub-Nagy <abanoubnagy24@gmail.com>
2023-03-26 06:19:39 +0200

    import glide libraries package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.fragment:fragment-ktx:1.5.5")
+    implementation("androidx.fragment:fragment-ktx:1.5.6")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
 
     // Glid
     implementation("com.github.bumptech.glide:glide:4.12.0")
