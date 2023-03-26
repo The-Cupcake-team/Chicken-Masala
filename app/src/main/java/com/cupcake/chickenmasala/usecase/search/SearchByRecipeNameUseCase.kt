1a6e1e23d01580c9b35e2381108591a5662f895d
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-03-26 10:30:24 +0200

    logic: add limit in use cases

--- app/src/main/java/com/cupcake/chickenmasala/usecase/search/SearchByRecipeNameUseCase.kt
+++ app/src/main/java/com/cupcake/chickenmasala/usecase/search/SearchByRecipeNameUseCase.kt
@@ -4,10 +4,11 @@ import com.cupcake.chickenmasala.data.model.Recipe
 import com.cupcake.chickenmasala.usecase.Repository
 
 class SearchByRecipeNameUseCase(private val repository: Repository) {
-    operator fun invoke(recipeName: String): List<Recipe>? {
+    operator fun invoke(recipeName: String, limit: Int = 100): List<Recipe>? {
         return repository
             .getRecipes()
             .filter { it.doesMatchRecipeName(recipeName) }
+            .take(limit)
             .takeIf { it.isNotEmpty() }
     }
 }
