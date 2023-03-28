45f209bb9e079d2130bc1ff55429b0a2cf4f77d8
Abanoub-Nagy <abanoubnagy24@gmail.com>
2023-03-28 23:09:02 +0200

    implementation for dishes fragment

---
+++ app/src/main/java/com/cupcake/chickenmasala/ui/fragment/dishes/DishesFragment.kt
@@ -0,0 +1,62 @@
+package com.cupcake.chickenmasala.ui.fragment.dishes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cupcake.chickenmasala.R
+import com.cupcake.chickenmasala.data.RepositoryImpl
+import com.cupcake.chickenmasala.data.model.Recipe
+import com.cupcake.chickenmasala.databinding.FragmentDishesBinding
+import com.cupcake.chickenmasala.ui.base.BaseFragment
+import com.cupcake.chickenmasala.ui.fragment.details.DetailsFragment
+import com.cupcake.chickenmasala.ui.fragment.dishes.adapter.DishesAdapter
+import com.cupcake.chickenmasala.ui.util.OnItemClickListener
+import com.cupcake.chickenmasala.usecase.Repository
+import com.cupcake.chickenmasala.usecase.dishes.GetAllRecipeFoodUseCase
+import com.cupcake.chickenmasala.utill.DataSourceProvider
+
+class DishesFragment : BaseFragment<FragmentDishesBinding>(), OnItemClickListener<Recipe> {
+    override val LOG_TAG = this::class.java.name
+
+    override val bindingInflater: (LayoutInflater, ViewGroup, Boolean) -> FragmentDishesBinding =
+        FragmentDishesBinding::inflate
+
+    private val repository: Repository by lazy {
+        RepositoryImpl(DataSourceProvider.getDataSource(requireActivity().application))
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupBackButton()
+        setUpDishesAdapter()
+    }
+
+    private fun setUpDishesAdapter() {
+        val dishes = GetAllRecipeFoodUseCase(repository).invoke(DISHES_LIMIT)
+        val adapter = DishesAdapter(this)
+        adapter.submitList(dishes)
+        binding.recyclerViewDishes.adapter = adapter
+    }
+
+
+    override fun onItemClicked(item: Recipe) {
+        val detailsFragment = DetailsFragment.newInstance(item.id)
+        requireActivity().supportFragmentManager.beginTransaction().apply {
+            add(R.id.fragmentContainer, detailsFragment)
+            addToBackStack(detailsFragment.javaClass.simpleName)
+            commit()
+        }
+    }
+
+    private fun setupBackButton() {
+        binding.toolbar.setNavigationOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+    }
+
+    private companion object {
+        const val DISHES_LIMIT = 100
+    }
+}
---
+++ app/src/main/java/com/cupcake/chickenmasala/ui/fragment/dishes/adapter/DishesAdapter.kt
@@ -0,0 +1,41 @@
+package com.cupcake.chickenmasala.ui.fragment.dishes.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.cupcake.chickenmasala.R
+import com.cupcake.chickenmasala.data.model.Recipe
+import com.cupcake.chickenmasala.databinding.RecipeCardVerticalBinding
+import com.cupcake.chickenmasala.ui.base.BaseAdapter
+import com.cupcake.chickenmasala.ui.util.OnItemClickListener
+import com.cupcake.chickenmasala.utill.setImage
+
+class DishesAdapter(private val listener: OnItemClickListener<Recipe>) :
+    BaseAdapter<Recipe, RecipeCardVerticalBinding>() {
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> RecipeCardVerticalBinding =
+        RecipeCardVerticalBinding::inflate
+
+    override fun <T> areItemsTheSame(
+        oldItemPosition: Int,
+        newItemPosition: Int,
+        newItems: List<T>
+    ) = getOldItems()[oldItemPosition].id == (newItems[newItemPosition] as Recipe).id
+
+    override fun bindItem(binding: RecipeCardVerticalBinding, item: Recipe) {
+        with(binding) {
+            val margin = root.resources.getDimensionPixelSize(R.dimen.spacing_small)
+            (root.layoutParams as ViewGroup.MarginLayoutParams).setMargins(
+                margin,
+                margin,
+                margin,
+                margin
+            )
+            cardImage.setImage(item.imageUrl)
+            recipeName.text = item.translatedRecipeName
+            cuisine.text = item.cuisine
+            time.text = item.totalTimeInMin.toString()
+            root.setOnClickListener {
+                listener.onItemClicked(item)
+            }
+        }
+    }
+}
