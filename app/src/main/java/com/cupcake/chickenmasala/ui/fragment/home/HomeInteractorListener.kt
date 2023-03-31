9a3d3deec16453a2ec83e1124bbab4d3c018d3dc
TarekIdrees <tarekdrias321@gmail.com>
2023-04-01 01:01:42 +0200

    change name of listener

--- app/src/main/java/com/cupcake/chickenmasala/ui/fragment/home/HomeInteractorListener.kt
+++ app/src/main/java/com/cupcake/chickenmasala/ui/fragment/home/HomeInteractorListener.kt
@@ -1,10 +1,8 @@
 package com.cupcake.chickenmasala.ui.fragment.home
 
-import com.google.android.material.chip.ChipGroup
-
 interface HomeInteractorListener {
     fun onViewPagerClicked(id: Int)
     fun onViewAllButtonClicked()
-    fun onCardClicked(id: Int)
+    fun onRecipeCardClicked(id: Int)
     fun onChipClicked(chipId: Int)
 }
