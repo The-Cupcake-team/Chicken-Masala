47397b9210d56739c805e5afe4414c0766bf812a
Hassan Ayman Ahmed <hassan3ayman@gmail.com>
2023-04-01 17:58:52 +0200

    delete the toFormatTime extension function

--- app/src/main/java/com/cupcake/chickenmasala/utill/Extention.kt
+++ app/src/main/java/com/cupcake/chickenmasala/utill/Extention.kt
@@ -15,16 +15,3 @@ fun ImageView.setImage(url: String) {
         .into(this)
 
 }
-
-fun Int.toFormatTime(): String{
-    val hours = this/60
-    val min = this % 60
-    var formatTime = ""
-    if (hours != 0){
-        formatTime = "${hours}h "
-    }
-    if (min != 0){
-        formatTime += "${min}m"
-    }
-    return formatTime
-}
